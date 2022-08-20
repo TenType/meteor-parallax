d7a9b1d26e21da3e9a8a2491750ac35d2fd38748
TenType <55125103+TenType@users.noreply.github.com>
2022-08-19 17:16:13 -0700

    Attempt to fix deployment 2

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,9 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  base: '/dist/',
-  plugins: [react()],
+export default defineConfig(({ mode }) => {
+  return {
+    base: mode == 'production' ? '/meteor-parallax/' : '/',
+    plugins: [react()],
+  };
 });
